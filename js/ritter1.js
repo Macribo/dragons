196de5bc254528c3aa9dec04b0ce8bf340d2589c
ribo <roibeard.mac.unfraidh@gmail.com>
2018-08-27 17:19:44 +0200

    Add first warning button hover effect.

--- js/ritter1.js
+++ js/ritter1.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     console.log('document is ready: hallo world');
 
+    console.log(name);
+    $(document).keypress(function(e) {
+        if(e.which == 13) {
+       
+    let name = $('#name-input').val();
+    console.log(name);
+            $('.erwachsene').addClass('hidden');
+            $('#ich-bin2').removeClass('hidden');
+            $('.jestem').css("background-image","url(./images/");
 
+
+
+}
+    });
 
     $('#abar1').click(function(){
         console.log('abar1 click');
@@ -9,6 +22,17 @@ $(document).ready(function() {
         $('.ritter2').style.display('block');
     
     });
+
+    $('.btn-warning').hover(function(){
+        $('.jesteye').removeClass('hidden');
+        $('.jesteye').removeClass('fader');
+
+    });
+    $('.btn-warning').mouseleave(function(){
+        console.log("cannoo");
+        $('#jesteye').addClass('fader');
+    });
+    
 
 });
 
