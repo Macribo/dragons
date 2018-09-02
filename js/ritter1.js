c93ea29dd4d387fe78cf7710304c5015be07e0fd
ribo <roibeard.mac.unfraidh@gmail.com>
2018-09-02 11:27:35 +0200

    Import tilt daemon.

--- js/ritter1.js
+++ js/ritter1.js
@@ -1,29 +1,8 @@
+
 $(document).ready(function() {
 
-clicked = false;
-    console.log('document is ready: hallo world');
-
-    console.log(name);
-    $(document).keypress(function(e) {
-        if(e.which == 13) {
-       
-    let name = $('#name-input').val();
-    console.log(name);
-            $('.erwachsene').addClass('hidden');
-            $('#ich-bin2').removeClass('hidden');
-            $('.jestem').css("background-image","url(./images/");
 
 
-
-}
-    });
-
-    $('#abar1').click(function(){
-        console.log('abar1 click');
-        $('.ritter1').style.display('none');
-        $('.ritter2').style.display('block');
-    
-    });
 
     $('.btn-warning').hover(function(){
         $('.jesteye').removeClass('hidden');
@@ -42,13 +21,83 @@ clicked = false;
         $('.jesteye').removeClass('hidden');
         $('.jesteye').removeClass('fader');
       
-            $('html, body').animate({
-                scrollTop: $(".du-bist").offset().top
-            }, 2000);
+            // $('html, body').animate({
+            //     scrollTop: $(".du-bist").offset().top
+            // }, 2000);
        
    
         });
 
+        
+        $('.eyes').tilt({
+            scale: 1.1
+        });
+    
+    
+        $("#eye1").hover(
+    
+            function () {
+                console.log("hover1");
+                $('.selectedLv').css("background-image", "url('./images/v1.png')");
+                $('.levelDescription').text(`Vorsicht: Diese App ändert das Bewusstsein`);
+            }
+        );
+        $("#eye2").hover(
+            function () {
+                console.log("hover2");
+                $('.selectedLv').css("background-image", "url('./images/v2.png')");
+                $('.levelDescription').text('Eine Theorie über Hypnose (Hinweis: Es ist nicht was du denkst; es ist was du denkst.)');
+            }
+        );
+        $("#eye2").click(function(){
+
+        });
+
+        $("#eye3").hover(
+            function () {
+                console.log("hover3");
+                $('.selectedLv').css("background-image", "url('./images/v3.png')");
+    
+                $('.levelDescription').text('Technologie, Magie und Realität: ein praktischer Rahmen');
+            }
+        );
+        $("#eye4").hover(
+            function () {
+                console.log("hover4");
+                $('.selectedLv').css("background-image", "url('./images/v4.png')");
+    
+                $('.levelDescription').text('Psychische Energie: Berühre die Welt mit deinem Gehirn');
+            }
+        );
+        $("#eye5").hover(
+            function () {
+                console.log("hover3");
+                $('.selectedLv').css("background-image", "url('./images/v5.png')");
+                $('.levelDescription').text('Kann jemand auf Deutsch auf Englisch denken? (Spoiler: \'No. \')');
+            }
+        );
+        $("#eye6").hover(
+            function () {
+                console.log("hover3");
+                $('.selectedLv').css("background-image", "url('./images/v6.png')");
+    
+                $('.levelDescription').text('Den hypnotischen Bildschirm durchstechen (ohne das Gerät zu beschädigen)');
+            }
+        );
+        $("#eye7").hover(
+            function () {
+                console.log("hover3");
+                $('.selectedLv').css("background-image", "url('./images/v7.png')");
+                $('.levelDescription').text('Weniger falsch sein: Selbst, Ego & der Prozess, unseren Geist zu verändern');
+            }
+        );
+        $("#eye8").hover(
+            function () {
+                console.log("hover3");
+                $('.selectedLv').css("background-image", "url('./images/v8.png')");
+                $('.levelDescription').text('Pfad des Narren');
+            }
+        );
 
    
 
---
+++ js/server.js
@@ -0,0 +1,4 @@
+let storyTexts = require("../public/story-texts");
+let story = 0;
+
+    $().innerHTML = storyTexts[story];
